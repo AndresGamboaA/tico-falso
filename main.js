8177673f29dd915b1a173062d3525ee0a81774ca
AndresGamboaA <andres24k8@gmail.com>
2022-05-29 20:52:21 -0600

    Add tables

--- main.js
+++ main.js
@@ -1,12 +1,28 @@
 import ticoFalso from "./src/tico-falso.js";
 
-document.getElementById("nombre").innerText = ticoFalso.persona();
-const provincia = ticoFalso.provincia();
-const canton = ticoFalso.canton(provincia);
-const distrito = ticoFalso.distrito(provincia, canton);
-document.getElementById("cedula").innerText = ticoFalso.cedula(provincia);
-document.getElementById("provincia").innerText = provincia;
-document.getElementById("canton").innerText = canton;
-document.getElementById("distrito").innerText = distrito;
-document.getElementById("direccion").innerText = ticoFalso.direccion(distrito);
-document.getElementById("codigo").innerText = ticoFalso.codigoPostal();
+const tabla = document.getElementById("tabla");
+
+for (let i=0; i<100;i++) {
+    const row = tabla.insertRow();
+    var c_cedula = row.insertCell();
+    var c_nombre = row.insertCell();
+    var c_provincia = row.insertCell();
+    var c_canton = row.insertCell();
+    var c_distrito = row.insertCell();
+    var c_direccion = row.insertCell();
+    var c_codigo = row.insertCell();
+
+    const provincia = ticoFalso.provincia();
+    const canton = ticoFalso.canton(provincia);
+    const distrito = ticoFalso.distrito(provincia, canton);
+    const cedula = ticoFalso.cedula(provincia);
+    const nombre = ticoFalso.persona();
+    const direccion = ticoFalso.direccion(distrito);
+    c_cedula.innerHTML = cedula;
+    c_nombre.innerHTML = nombre;
+    c_canton.innerHTML = canton;
+    c_provincia.innerHTML = provincia;
+    c_direccion.innerHTML = direccion;
+    c_distrito.innerHTML = distrito;
+    c_codigo.innerHTML = ticoFalso.codigoPostal();
+}
