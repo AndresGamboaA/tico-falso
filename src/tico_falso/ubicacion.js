c3229c05acf824e084a96ead4eb5f6d6a27a7f47
AndresGamboaA <andres24k8@gmail.com>
2022-05-31 15:34:53 -0600

    Remove redundant funtions

--- src/tico_falso/ubicacion.js
+++ src/tico_falso/ubicacion.js
@@ -8,24 +8,17 @@ Ubicacion.provincia = function() {
     return Utils.aleatorio(provincias).nombre;
 };
 
-Ubicacion.canton = function() {
-    const provincia = Utils.aleatorio(provincias)
+Ubicacion.canton = function(nombreProvincia=null) {
+    const provincia = nombreProvincia?
+        provincias.find(provincia => provincia.nombre == nombreProvincia):
+        Utils.aleatorio(provincias);
     return Utils.aleatorio(provincia.cantones).nombre;
 };
 
-Ubicacion.cantonDe = function(nombreProvincia) {
-    const provincia = provincias.find(provincia => provincia.nombre == nombreProvincia);
-    return Utils.aleatorio(provincia.cantones).nombre;
-};
-
-Ubicacion.distrito = function() {
-    let provincia = Utils.aleatorio(provincias);
-    let canton = Utils.aleatorio(provincia.cantones);
-    return Utils.aleatorio(canton.distritos);
-};
-
-Ubicacion.distritoDe = function(nombreProvincia,nombreCanton=null) {
-    let provincia = provincias.find(item => item.nombre == nombreProvincia);
+Ubicacion.distrito = function(nombreProvincia,nombreCanton=null) {
+    let provincia = nombreProvincia?
+        provincias.find(item => item.nombre == nombreProvincia):
+        Utils.aleatorio(provincias);
 
     let canton = nombreCanton?
         provincia.cantones.find(item => item.nombre == nombreCanton):
@@ -38,12 +31,8 @@ Ubicacion.codigoPostal = function() {
     return Math.floor(Math.random() * (70605 - 10101) ) + 10101;
 };
 
-Ubicacion.direccionDe = function(pueblo=null) {
-    return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${pueblo}.`;
+Ubicacion.direccion = function(pueblo=null) {
+    return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${pueblo?pueblo:Ubicacion.distrito()}.`;
 }
-
-Ubicacion.direccion = function() {
-    return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${Ubicacion.distrito()}.`;
-};
 
 export default Ubicacion;
