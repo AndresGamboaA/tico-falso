11be73c78af4e3cce0809aadbf3f1881ca1c00af
AndresGamboaA <andres24k8@gmail.com>
2022-06-01 13:13:25 -0600

    Genarate iban numbers

--- dist/tico_falso.js
+++ dist/tico_falso.js
@@ -1,8 +1,10 @@
 (function (global, factory) {
    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
    typeof define === 'function' && define.amd ? define(factory) :
-   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global["tico-falso"] = factory());
+   (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.ticoFalso = factory());
 })(this, (function () { 'use strict';
+
+   //https://nombresdepersona.com/costa-rica
 
    var nombresHombres = [
       'Carlos',
@@ -14,7 +16,6 @@
       'Mario',
       'Oscar',
       'Manuel',
-      'Carmen',
       'Miguel',
       'Francisco',
       'Marco',
@@ -68,6 +69,8 @@
       'Rolando',
       'German'
    ];
+
+   //https://nombresdepersona.com/costa-rica
 
    var nombresMujeres = [
        'María',
@@ -277,61 +280,6 @@
        'Murillo',
        'Soto'
    ];
-
-   var Utils = {};
-
-   Utils.aleatorio = function (array) {
-       return array[Math.floor(Math.random() * array.length)];
-   };
-
-   var Personas = {};
-
-   Personas.nombreHombre = function () {
-      return Utils.aleatorio(nombresHombres);
-   };
-
-   Personas.nombreMujer = function () {
-      return Utils.aleatorio(nombresMujeres);
-   };
-
-   Personas.tico = function () {
-      return `${Utils.aleatorio(nombresHombres)} ${Utils.aleatorio(
-      nombresHombres
-   )} ${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
-   };
-
-   Personas.tica = function () {
-      return `${Utils.aleatorio(nombresMujeres)} ${Utils.aleatorio(
-      nombresMujeres
-   )} ${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
-   };
-
-   Personas.persona = function () {
-      return Math.random() > 0.5 ? Personas.tico() : Personas.tica();
-   };
-
-   Personas.cedula = function (provincia = null) {
-      var p_valor = Utils.aleatorio([1, 2, 3, 4, 5, 6, 7]);
-      if (provincia) {
-         p_valor =
-            [
-               "San José",
-               "Alajuela",
-               "Cartago",
-               "Heredia",
-               "Guanacaste",
-               "Puntarenas",
-               "Limón",
-            ].indexOf(provincia) + 1;
-      }
-      if (p_valor == 0) {
-         throw new Error("Provincia no válida. ¿La escribio bien?");
-      }
-      return Number(
-         p_valor +
-            String(Math.floor(Math.random() * (99999999 - 11111111)) + 99999999)
-      );
-   };
 
    var provincias = [
        {
@@ -1365,30 +1313,29 @@
        ]
    };
 
+   var Utils = {};
+
+   Utils.aleatorio = function (array) {
+       return array[Math.floor(Math.random() * array.length)];
+   };
+
    var Ubicacion = {};
 
    Ubicacion.provincia = function() {
        return Utils.aleatorio(provincias).nombre;
    };
 
-   Ubicacion.canton = function() {
-       const provincia = Utils.aleatorio(provincias);
+   Ubicacion.canton = function(nombreProvincia=null) {
+       const provincia = nombreProvincia?
+           provincias.find(provincia => provincia.nombre == nombreProvincia):
+           Utils.aleatorio(provincias);
        return Utils.aleatorio(provincia.cantones).nombre;
    };
 
-   Ubicacion.cantonDe = function(nombreProvincia) {
-       const provincia = provincias.find(provincia => provincia.nombre == nombreProvincia);
-       return Utils.aleatorio(provincia.cantones).nombre;
-   };
-
-   Ubicacion.distrito = function() {
-       let provincia = Utils.aleatorio(provincias);
-       let canton = Utils.aleatorio(provincia.cantones);
-       return Utils.aleatorio(canton.distritos);
-   };
-
-   Ubicacion.distritoDe = function(nombreProvincia,nombreCanton=null) {
-       let provincia = provincias.find(item => item.nombre == nombreProvincia);
+   Ubicacion.distrito = function(nombreProvincia,nombreCanton=null) {
+       let provincia = nombreProvincia?
+           provincias.find(item => item.nombre == nombreProvincia):
+           Utils.aleatorio(provincias);
 
        let canton = nombreCanton?
            provincia.cantones.find(item => item.nombre == nombreCanton):
@@ -1401,12 +1348,110 @@
        return Math.floor(Math.random() * (70605 - 10101) ) + 10101;
    };
 
-   Ubicacion.direccionDe = function(pueblo=null) {
-       return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${pueblo}.`;
+   Ubicacion.direccion = function(pueblo=null) {
+       return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${pueblo?pueblo:Ubicacion.distrito()}.`;
    };
 
-   Ubicacion.direccion = function() {
-       return `${Utils.aleatorio(senales.metros)} metros ${Utils.aleatorio(senales.puntosCardinales)} ${Utils.aleatorio(senales.puntosReferencia)} de ${Ubicacion.distrito()}.`;
+   var Personas = {};
+
+   Personas.nombre = function(femenino=false) {
+      if (femenino)
+         return Utils.aleatorio(nombresMujeres);
+      return Utils.aleatorio(nombresHombres);
+   };
+
+   Personas.apellido = function() {
+      return Utils.aleatorio(apellidos);
+   };
+
+   Personas.nombreHombre = function () {
+      return Utils.aleatorio(nombresHombres);
+   };
+
+   Personas.nombreMujer = function () {
+      return Utils.aleatorio(nombresMujeres);
+   };
+
+   Personas.tico = function(apellidosPrimero=false) {
+      const nombre = `${Utils.aleatorio(nombresHombres)} ${Utils.aleatorio(nombresHombres)}`;
+      const tico_apellidos = `${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+      if (apellidosPrimero)
+         return tico_apellidos + " " + nombre;
+      return nombre + " " + tico_apellidos;
+   };
+
+   Personas.tica = function(apellidosPrimero=false) {
+      const nombre = `${Utils.aleatorio(nombresMujeres)} ${Utils.aleatorio(nombresMujeres)}`;
+      const tico_apellidos = `${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+      if (apellidosPrimero)
+         return tico_apellidos + " " + nombre;
+      return nombre + " " + tico_apellidos;
+   };
+
+   Personas.persona = function (apellidosPrimero=false) {
+      return Math.random() > 0.5 ? Personas.tico(apellidosPrimero) : Personas.tica(apellidosPrimero);
+   };
+
+   Personas.datos = function() {
+      const sexo = Math.random() > 0.5 ? "masculino":"femenino";
+      const provincia = Ubicacion.provincia();
+      const canton = Ubicacion.canton(provincia);
+      const distrito = Ubicacion.distrito(provincia, canton);
+      const direccion = Ubicacion.direccion(distrito);
+
+      return {
+         nombre: Personas.nombre(sexo==="femenino") +  " " + Personas.nombre(sexo==="femenino"),
+         apellidos: Personas.apellido() + " " + Personas.apellido(),
+         sexo: sexo,
+         cedula: Personas.cedula(true, provincia),
+         provincia: provincia,
+         canton: canton,
+         distrito: distrito,
+         direccion: direccion,
+         iban: Personas.iban(),
+         telefono: Personas.telefono()
+      }
+   };
+
+   Personas.cedula = function (formateada=true, provincia = null) {
+      var p_valor = Utils.aleatorio([1, 2, 3, 4, 5, 6, 7]);
+      if (provincia) {
+         p_valor =
+            [
+               "San José",
+               "Alajuela",
+               "Cartago",
+               "Heredia",
+               "Guanacaste",
+               "Puntarenas",
+               "Limón",
+            ].indexOf(provincia) + 1;
+      }
+      if (p_valor == 0) {
+         throw new Error("Provincia no válida.");
+      }
+      var tomo = Math.floor(Math.random() * 9999);
+      var asiento = Math.floor(Math.random() * 9999);
+
+      if (formateada) {
+         return `0${p_valor}-${String(tomo).padStart(4, '0')}-${String(asiento).padStart(4, '0')}`;
+      }
+      return `0${p_valor}${String(tomo).padStart(4, '0')}${String(asiento).padStart(4, '0')}`;
+   };
+   //      0011000011111100000000
+   // IBAN CR06010200009123456789
+   Personas.iban = function() {
+      const codigoPais = "CR";
+      const verificador = String(Math.floor(Math.random()*99)).padStart(2, '0');
+      const banco = Utils.aleatorio(['100','102','104','107','123','151','152','161']);
+      const cuenta1 = String(Math.floor(Math.random() * (19999999 - 10000000) ) + 100000000).padStart(8, '0');
+      const cuenta2 = String(Math.floor(Math.random() * 99999)).padStart(5, '0');
+      return codigoPais + verificador + "0" + banco + cuenta1 + cuenta2;
+   };
+
+   Personas.telefono = function() {
+      const telefonica = Utils.aleatorio([6,8]);
+      return Number(""+telefonica+(String(Math.floor(Math.random() * 9999999)).padStart(7, '0')));
    };
 
    const ticoFalso = {
--- src/tico_falso/personas.js
+++ src/tico_falso/personas.js
@@ -1,9 +1,20 @@
 import nombresHombres from "./datos/nombres_hombres";
 import nombresMujeres from "./datos/nombres_mujeres";
 import apellidos from "./datos/apellidos";
+import Ubicacion from "./ubicacion";
 import Utils from "./utils";
 
 var Personas = {};
+
+Personas.nombre = function(femenino=false) {
+   if (femenino)
+      return Utils.aleatorio(nombresMujeres);
+   return Utils.aleatorio(nombresHombres);
+}
+
+Personas.apellido = function() {
+   return Utils.aleatorio(apellidos);
+}
 
 Personas.nombreHombre = function () {
    return Utils.aleatorio(nombresHombres);
@@ -13,23 +24,48 @@ Personas.nombreMujer = function () {
    return Utils.aleatorio(nombresMujeres);
 };
 
-Personas.tico = function () {
-   return `${Utils.aleatorio(nombresHombres)} ${Utils.aleatorio(
-      nombresHombres
-   )} ${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+Personas.tico = function(apellidosPrimero=false) {
+   const nombre = `${Utils.aleatorio(nombresHombres)} ${Utils.aleatorio(nombresHombres)}`;
+   const tico_apellidos = `${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+   if (apellidosPrimero)
+      return tico_apellidos + " " + nombre;
+   return nombre + " " + tico_apellidos;
 };
 
-Personas.tica = function () {
-   return `${Utils.aleatorio(nombresMujeres)} ${Utils.aleatorio(
-      nombresMujeres
-   )} ${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+Personas.tica = function(apellidosPrimero=false) {
+   const nombre = `${Utils.aleatorio(nombresMujeres)} ${Utils.aleatorio(nombresMujeres)}`;
+   const tico_apellidos = `${Utils.aleatorio(apellidos)} ${Utils.aleatorio(apellidos)}`;
+   if (apellidosPrimero)
+      return tico_apellidos + " " + nombre;
+   return nombre + " " + tico_apellidos;
 };
 
-Personas.persona = function () {
-   return Math.random() > 0.5 ? Personas.tico() : Personas.tica();
+Personas.persona = function (apellidosPrimero=false) {
+   return Math.random() > 0.5 ? Personas.tico(apellidosPrimero) : Personas.tica(apellidosPrimero);
 };
 
-Personas.cedula = function (provincia = null) {
+Personas.datos = function() {
+   const sexo = Math.random() > 0.5 ? "masculino":"femenino";
+   const provincia = Ubicacion.provincia();
+   const canton = Ubicacion.canton(provincia);
+   const distrito = Ubicacion.distrito(provincia, canton);
+   const direccion = Ubicacion.direccion(distrito);
+
+   return {
+      nombre: Personas.nombre(sexo==="femenino") +  " " + Personas.nombre(sexo==="femenino"),
+      apellidos: Personas.apellido() + " " + Personas.apellido(),
+      sexo: sexo,
+      cedula: Personas.cedula(true, provincia),
+      provincia: provincia,
+      canton: canton,
+      distrito: distrito,
+      direccion: direccion,
+      iban: Personas.iban(),
+      telefono: Personas.telefono()
+   }
+}
+
+Personas.cedula = function (formateada=true, provincia = null) {
    var p_valor = Utils.aleatorio([1, 2, 3, 4, 5, 6, 7]);
    if (provincia) {
       p_valor =
@@ -44,12 +80,30 @@ Personas.cedula = function (provincia = null) {
          ].indexOf(provincia) + 1;
    }
    if (p_valor == 0) {
-      throw new Error("Provincia no válida. ¿La escribio bien?");
+      throw new Error("Provincia no válida.");
    }
-   return Number(
-      p_valor +
-         String(Math.floor(Math.random() * (99999999 - 11111111)) + 99999999)
-   );
+   var tomo = Math.floor(Math.random() * 9999);
+   var asiento = Math.floor(Math.random() * 9999);
+
+   if (formateada) {
+      return `0${p_valor}-${String(tomo).padStart(4, '0')}-${String(asiento).padStart(4, '0')}`;
+   }
+   return `0${p_valor}${String(tomo).padStart(4, '0')}${String(asiento).padStart(4, '0')}`;
 };
+//      0011000011111100000000
+// IBAN CR06010200009123456789
+Personas.iban = function() {
+   const codigoPais = "CR";
+   const verificador = String(Math.floor(Math.random()*99)).padStart(2, '0');
+   const banco = Utils.aleatorio(['100','102','104','107','123','151','152','161']);
+   const cuenta1 = String(Math.floor(Math.random() * (19999999 - 10000000) ) + 100000000).padStart(8, '0');
+   const cuenta2 = String(Math.floor(Math.random() * 99999)).padStart(5, '0');
+   return codigoPais + verificador + "0" + banco + cuenta1 + cuenta2;
+}
+
+Personas.telefono = function() {
+   const telefonica = Utils.aleatorio([6,8]);
+   return Number(""+telefonica+(String(Math.floor(Math.random() * 9999999)).padStart(7, '0')));
+}
 
 export default Personas;
